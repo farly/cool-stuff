c18dfdb488e35d28e2b3c7d2efbdcb37668a8911
Vincent Farly Taboada <vftaboada@gmail.com>
2024-07-30 19:21:12 +0800

    scheduler algorithm

--- scheduler.test.ts
+++ scheduler.test.ts
@@ -1,5 +1,218 @@
-import Scheduler from "./scheduler";
+import Scheduler, { Availability, Day, Lesson } from "./scheduler";
 
 describe("Scheduler challenge tests", () => {
-    // TODO Your test scripts goes here
+  test('when lessons is empty should return empty schedule', () => {
+
+    const availabilities: Availability[] = [{
+      id: 1,
+      day: 0,
+      startTime: "10:00",
+      endTime: "12:00" 
+    }]
+
+    const scheduler = new Scheduler()
+    const schedules = scheduler.schedule([], availabilities)
+    
+    expect(schedules).toStrictEqual([])
+  })
+
+  test('when availabities is empty should return empty schedule', () => {
+    const lesson: Lesson[] = [{
+      title: 'Title',
+      duration: 30 
+    }]
+
+    const scheduler = new Scheduler()
+    const schedules = scheduler.schedule(lesson, [])
+
+    expect(schedules).toStrictEqual([])
+  })
+
+  test('when total lesson duration is equal to availability duration', () => {
+    const lessons = [{
+      title: 'JS Basics',
+      duration: 90,
+    }, {
+      title: 'JS Fundamentals',
+      duration: 90,
+    }]
+
+    const availabilities = [{
+      id: 1,
+      day: 1 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }, {
+      id: 2,
+      day: 2 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }, {
+      id: 3,
+      day: 2 as Day,
+      startTime: '13:00',
+      endTime: '14:00',
+    }]
+
+    const expectedOutput =  [{
+      "availabilityId": 1,
+      "lessons": [{
+        "title": "JS Basics",
+        "duration": 60
+      }]
+      }, {
+        "availabilityId": 2,
+        "lessons": [{
+          "title": "JS Basics",
+          "duration": 30
+        }, {
+          "title": "JS Fundamentals",
+          "duration": 30
+        }]
+      }, {
+        "availabilityId": 3,
+        "lessons": [{
+          "title": "JS Fundamentals",
+          "duration": 60
+        }] 
+      }]
+
+    const scheduler = new Scheduler()
+    const schedules = scheduler.schedule(lessons, availabilities)
+    expect(schedules).toEqual(expectedOutput)
+  })
+
+  test('when total lesson duration is less than to availability duration', () => {
+    const lessons = [{
+      title: 'Lesson 1',
+      duration: 30,
+    }, {
+      title: 'Lesson 2',
+      duration: 30,
+    }]
+
+    const availabilities = [{
+      id: 1,
+      day: 1 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }, {
+      id: 2,
+      day: 2 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }, {
+      id: 3,
+      day: 2 as Day,
+      startTime: '13:00',
+      endTime: '14:00',
+    }]
+
+    const expectedOutput =  [{
+      "availabilityId": 1,
+      "lessons": [{
+        "title": "Lesson 1",
+        "duration": 30 
+      }, {
+        "title": "Lesson 2",
+        "duration": 30 
+      }]
+    }]
+      
+    const scheduler = new Scheduler()
+    const schedules = scheduler.schedule(lessons, availabilities)
+    expect(schedules).toEqual(expectedOutput)
+  })
+
+  test('when total lesson duration greater than to availability duration', () => {
+    const lessons = [{
+      title: 'Lesson 1',
+      duration: 90,
+    }, {
+      title: 'Lesson 2',
+      duration: 90,
+    }]
+
+    const availabilities = [{
+      id: 1,
+      day: 1 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }, {
+      id: 2,
+      day: 2 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }]
+
+    const expectedOutput =  [{
+      "availabilityId": 1,
+      "lessons": [{
+        "title": "Lesson 1",
+        "duration": 60 
+      }]
+    }, {
+      "availabilityId": 2,
+      "lessons": [{
+        "title": "Lesson 1",
+        "duration": 30 
+      }, {
+        "title": "Lesson 2",
+        "duration": 30 
+      }]
+    }]
+      
+    const scheduler = new Scheduler()
+    const schedules = scheduler.schedule(lessons, availabilities)
+    expect(schedules).toEqual(expectedOutput)
+  })
+
+  test('should return correct reschedule', () => {
+    const lessons = [{
+      title: 'Lesson 1',
+      duration: 15,
+    }, {
+      title: 'Lesson 2',
+      duration: 30,
+    }, {
+      title: 'Lesson 3',
+      duration: 20
+    }]
+
+    const availabilities = [{
+      id: 1,
+      day: 1 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }, {
+      id: 2,
+      day: 2 as Day,
+      startTime: '09:00',
+      endTime: '10:00',
+    }]
+
+    const expectedOutput =  [{
+      "availabilityId": 1,
+      "lessons": [{
+        "title": "Lesson 1",
+        "duration": 15 
+      }, {
+        "title": "Lesson 2",
+        "duration": 30 
+      }, {
+        "title": "Lesson 3",
+        "duration": 15 
+      }]
+    }, {
+      "availabilityId": 2,
+      "lessons": [{
+        "title": "Lesson 3",
+        "duration": 5 
+      }]
+    }]
+      
+    const scheduler = new Scheduler()
+    const schedules = scheduler.schedule(lessons, availabilities)
+    expect(schedules).toEqual(expectedOutput)
+  })
 });
--- scheduler.ts
+++ scheduler.ts
@@ -1,19 +1,102 @@
-type Day = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+export type Day = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
-export type Lesson = {};
+export type Lesson = {
+  title: string
+  duration: number
+};
 
-export type Availability = {};
+export type Availability = {
+  id: number
+  day: Day
+  startTime: string
+  endTime: string
+};
 
-export type Schedule = {};
+export type Schedule = {
+  availabilityId: number
+  lessons: Lesson[]
+};
+
+function strToTime(time: string): {hours: number, mins: number} {
+  const [hours, mins] = time.split(":") 
+  return { hours: parseInt(hours), mins: parseInt(mins)}
+}
+
 
 class Scheduler {
-    constructor() {}
+  constructor() {}
 
-    schedule(lessons: Lesson[], availabilities: Availability[]): Schedule[] {
-        // TODO implement algorithm for scheduling here
+  schedule(lessons: Lesson[], availabilities: Availability[]): Schedule[] {
+    const schedules : Schedule[] = []
+    
+    let i = 0 // availabities index
+    let j = 0 // lessons index
+    const availabilitiesLength = availabilities.length
+    const lessonsLength = lessons.length
 
-        return [];
+    const lessonDurations = this.retrieveLessonDuration(lessons)
+    const availabilitiesDurations = this.retrieveAvailabityDuration(availabilities)
+
+    while(i < availabilitiesLength && j < lessonsLength) {
+      const lessonDuration = lessonDurations[j]
+      const availabilityDuration = availabilitiesDurations[i]
+
+      const availability = availabilities[i] 
+      const lesson = lessons[j]
+
+      if (!schedules.some(s => s.availabilityId === availability.id)) {
+        schedules.push({
+          availabilityId: availability.id,
+          lessons: []
+        })
+      } 
+
+      const schedule = schedules.find(s => availability.id === s.availabilityId)
+      const duration = lessonDuration >= availabilityDuration ? availabilityDuration : lessonDuration
+
+      schedule?.lessons.push({
+        title: lesson.title,
+        duration
+      })
+
+     if (duration === lessonDuration) {
+        availabilitiesDurations[i] = availabilityDuration - duration 
+        lessonDurations[j] = 0 
+        j++
+      } else if (duration === availabilityDuration) {
+        lessonDurations[j] = lessonDuration - duration 
+        availabilitiesDurations[i] = 0 
+        i++
+      }
+
+
     }
+
+
+    return schedules
+  }
+
+  retrieveLessonDuration(lessons: Lesson[]): number[] {
+    return lessons.map(lesson => lesson.duration)
+  } 
+
+  retrieveAvailabityDuration(availabilities: Availability[]): number[] {
+    return availabilities.map(a => {
+      const { hours: startHours, mins: startMin } = strToTime(a.startTime)
+      const { hours: endHours, mins: endMin } = strToTime(a.endTime)
+
+      const startTime = new Date()
+      startTime.setHours(startHours)
+      startTime.setMinutes(startMin)
+
+      const endTime = new Date()
+      endTime.setHours(endHours)
+      endTime.setMinutes(endMin)
+
+      const availabilityDuration = Math.ceil((endTime.valueOf() - startTime.valueOf()) / (60 * 1000))
+      return availabilityDuration
+    })
+  }
 }
 
 export default Scheduler;
